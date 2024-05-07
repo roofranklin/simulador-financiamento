163beed41b2a43c2c96cbd48580d4effca65943b
Roosevelt Franklin <roosevelt_franklin@hotmail.com>
2024-05-07 17:16:25 -0300

    Rotas ok

--- simulador-financiamento/src/app/app.routes.ts
+++ simulador-financiamento/src/app/app.routes.ts
@@ -1,3 +1,16 @@
-import { Routes } from '@angular/router';
+import { Routes, RouterModule } from '@angular/router';
+import { NgModule } from '@angular/core';
 
-export const routes: Routes = [];
+import { FormularioComponent } from './paginas/formulario/formulario.component';
+import { ResultadoComponent } from './paginas/resultado/resultado.component';
+
+export const routes: Routes = [
+    { path: '', component: FormularioComponent },
+    { path: 'resultado', component: ResultadoComponent }
+];
+
+@NgModule({
+    imports: [RouterModule.forRoot(routes)],
+    exports: [RouterModule]
+})
+export class AppRoutingModule { }
