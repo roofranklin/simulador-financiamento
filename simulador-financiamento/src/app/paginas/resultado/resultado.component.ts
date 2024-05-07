78808a725c4608401370bb40fc2df8785d815860
Roosevelt Franklin <roosevelt_franklin@hotmail.com>
2024-05-07 17:12:34 -0300

    Componentes criados

---
+++ simulador-financiamento/src/app/paginas/resultado/resultado.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-resultado',
+  standalone: true,
+  imports: [],
+  templateUrl: './resultado.component.html',
+  styleUrl: './resultado.component.scss'
+})
+export class ResultadoComponent {
+
+}
